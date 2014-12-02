4dc54b5b914284321b16cad817085ec719c6424f
efimikvitaliy <vitaliy1993@mail.ru>
2014-12-02 15:38:05 +0300

    Update

--- Bank.java
+++ Bank.java
@@ -1,7 +1,11 @@
+import Application.Controllers.LoginController;
+
 
 public class Bank {
-  
+	
 	public static void main(String[] args){
+		System.out.println("Hello");
+		LoginController loginController = new LoginController();
 		
 	}
 	
