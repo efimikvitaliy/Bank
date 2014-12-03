2b316a345dc45ccbce2f7bfdd8b1c531d3d1b54c
zhukalexey <zhukalexe@mail.ru>
2014-12-03 20:22:35 +0300

    Update LoginForm.java

--- Application/Forms/LoginForm.java
+++ Application/Forms/LoginForm.java
@@ -1,158 +1,179 @@
 package Application.Forms;
 
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.BorderFactory;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.sql.SQLException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
-import javax.swing.border.Border;
+import Application.Controllers.LoginController;
 
-import BusinessService.Entities.*;
-import Application.Controllers.*;
-import Application.Forms.*;
-import Database.*;
+public class LoginForm
+{
+	private LoginController m_LoginController;
+	private JFrame jf;
+	private JButton jb1;
+	private JButton jb2;
+	private JLabel jl1;
+	private JLabel jl2;
+	private JTextField jtf1;
+	private JTextField jtf2;
 
-
-
-/**
- * @author User
- * @version 1.0
- * @created 01-дек-2014 23:49:14
- */
-public class LoginForm  {
-
-	public LoginController m_LoginController;
-	JFrame loginForm;
-	
-	public LoginForm(LoginController loginController){
-		m_LoginController = loginController;
-		loginForm = new JFrame("Login");
+	public LoginForm()
+	{
+		jf = new JFrame("Login");
+		jf.setSize(600,600);
+		jf.setLocation(100, 100);
+		jf.setResizable(false);
+		GridBagLayout gbl = new GridBagLayout();
+		GridBagConstraints gbc = new GridBagConstraints();
+		jf.setLayout(gbl);
+		jf.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		
-		System.out.println("Hello from login");
-		init();
-	}
-
-	public void init(){
-		JPanel panel = new JPanel(), loginPanel = new JPanel(), passwordPanel = new JPanel(), buttonPanel = new JPanel();
-		JButton loginButton = new JButton("Login");
-		loginButton.addActionListener(new ActionListener(){
-
+		jl1 = new JLabel("Login: ");
+		gbc.insets = new Insets(0,0,50,0);
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		gbl.setConstraints(jl1, gbc);
+		jf.add(jl1);
+		
+		jtf1 = new JTextField("", 20);
+		gbc.insets = new Insets(0,50,50,0);
+		gbc.gridx = 2;
+		gbc.gridy = 1;
+		gbl.setConstraints(jtf1, gbc);
+		jf.add(jtf1);
+		
+		jl2 = new JLabel("Password: ");
+		gbc.insets = new Insets(0,0,50,0);
+		gbc.gridx = 1;
+		gbc.gridy = 2;
+		gbl.setConstraints(jl2, gbc);
+		jf.add(jl2);
+		
+		jtf2 = new JTextField("", 20);
+		gbc.insets = new Insets(0,50,50,0);
+		gbc.gridx = 2;
+		gbc.gridy = 2;
+		gbl.setConstraints(jtf2, gbc);
+		jf.add(jtf2);
+		
+		jb1 = new JButton("OK");
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 1;
+		gbc.gridy = 3;
+		gbl.setConstraints(jb1, gbc);
+		jf.add(jb1);
+		
+		jb2 = new JButton("Exit");
+		gbc.gridx = 1;
+		gbc.gridy = 4;
+		gbl.setConstraints(jb2, gbc);
+		jf.add(jb2);
+		
+		jb1.addActionListener(new ActionListener()
+		{
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
-				
-				
-			}});
-		
-		JTextField loginPane = new JTextField();
-		JTextField passwordPane = new JTextField();
-		panel.setLayout(new GridLayout(0,1,10,10));
-		loginPanel.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.HORIZONTAL; 
-		c.weightx = 0.3;
-		c.weighty = 0.5;
-		c.gridx = 1; 
-	    c.gridy = 0; 
-		loginPanel.add(new JLabel(""), c); 
-		c.weightx = 0.2;
-        c.gridx = 2; 
-        c.gridy = 0; 
-		panel.add(new JLabel());
-		loginPanel.add(new JLabel("Login:"), c); 
-		c.weightx = 0.8;
-		c.gridx = 3; 
-        c.gridy = 0; 
-		loginPanel.add(loginPane, c);
-		c.weightx = 0.3;
-		c.gridx = 4; 
-        c.gridy = 0; 
-        loginPanel.add(new JLabel(""), c); 
-        c.weightx = 0.3;
-		c.gridx = 1; 
-	    c.gridy = 0; 
-		loginPanel.add(new JLabel(""), c); 
-		c.weightx = 0.2;
-        c.gridx = 2; 
-        c.gridy = 0; 
-		loginPanel.add(new JLabel("Login:"), c); 
-		c.weightx = 0.8;
-		c.gridx = 3; 
-        c.gridy = 0; 
-		loginPanel.add(loginPane, c);
-		c.weightx = 0.8;
-		c.gridx = 4; 
-        c.gridy = 0; 
-        loginPanel.add(new JLabel(""), c); 
-        c.weightx = 0.3;
-		c.gridx = 1; 
-	    c.gridy = 2; 
-		loginPanel.add(new JLabel(""), c); 
-		c.weightx = 0.2;
-        c.gridx = 2; 
-        c.gridy = 2; 
-		loginPanel.add(new JLabel("Password:"), c); 
-		c.weightx = 0.8;
-		c.gridx = 3; 
-        c.gridy = 2; 
-		loginPanel.add(passwordPane, c);
-		c.weightx = 0.3;
-		c.gridx = 4; 
-        c.gridy = 2; 
-        loginPanel.add(new JLabel(""), c); 
-        c.weightx = 0.5;
-		c.gridx = 1; 
-        c.gridy = 3; 
-        loginPanel.add(new JLabel(""), c); 
-        c.weightx = 0.3;
-		c.gridx = 3; 
-        c.gridy = 3; 
-        loginPanel.add(loginButton, c); 
-       
-        panel.add(loginPanel);
-		panel.add(new JLabel());
-		loginForm.add(panel);
-		loginForm.setSize(500,500);
-		
-		System.out.println("Hello from init");
+			public void actionPerformed(ActionEvent e)
+			{
+				if(jtf1.getText().trim().length() == 0)
+				{
+					JOptionPane.showMessageDialog(jf, "Entered your name");
+				}
+				else if(jtf2.getText().trim().length() == 0)
+				{
+					JOptionPane.showMessageDialog(jf, "Entered your password");
+				}
+				else
+				{
+					try
+					{
+						m_LoginController = new LoginController();
+						m_LoginController.login(jtf1.getText().trim(), 
+
+jtf2.getText().trim());
+						boolean b = 
+
+m_LoginController.isManager(m_LoginController.getUser().getType());
+						if(b)
+						{
+							jf.setVisible(false);
+						}
+						else
+						{
+							JOptionPane.showMessageDialog(jf, "User not found");
+						}
+					}
+					catch(SQLException e1)
+					{
+						JOptionPane.showMessageDialog(jf, "DataBase is not available");
+					}
+				}
+			}
+		});
+		jb2.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				int res = JOptionPane.showConfirmDialog(jf, "Do you want exit?", "Exit", 
+
+JOptionPane.YES_NO_OPTION);
+				switch(res)
+				{
+				case JOptionPane.YES_OPTION:
+					System.exit(0);
+					break;
+				case JOptionPane.NO_OPTION:
+					break;
+				}
+			}
+		});
+		jf.addWindowListener(new WindowAdapter()
+		{
+			@Override
+			public void windowClosing(WindowEvent e)
+			{
+				int res = JOptionPane.showConfirmDialog(jf, "Do you want exit?", "Exit", 
+
+JOptionPane.YES_NO_OPTION);
+				switch(res)
+				{
+				case JOptionPane.YES_OPTION:
+					System.exit(0);
+					break;
+				case JOptionPane.NO_OPTION:
+					break;
+				}
+			}
+		});
 	}
-	/*public void finalize() throws Throwable {
-
-	}*/
-
-	/**
-	 * 
-	 * @param b
-	 */
-	public void setVisible(boolean b){
-		loginForm.setVisible(b);
+	public void setVisible(boolean b)
+	{
+		jf.setVisible(b);
+	}
+	public static void showInterfaceOfCatalogManager()
+	{
 		
 	}
-
-	public void showInterfaceOfCatalogManager(){
+	public static void showInterfaceOfClientManager()
+	{
+		new ClientManagerForm().setVisible(true);
+	}
+	public static void showInterfaceOfManufactureManager()
+	{
+		new ManufactureManagerForm().setVisible(true);
+	}
+	public static void showInterfaceOfTransportManager()
+	{
 
 	}
-
-	public void showInterfaceOfClientManager(){
-
-	}
-
-	public void showInterfaceOfManufactureManager(){
-
-	}
-
-	public void showInterfaceOfTransportManager(){
-
-	}
-
 }
