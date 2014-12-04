333c23536727cf02ba367aeda107f34e0025e802
zhukalexey <zhukalexe@mail.ru>
2014-12-04 19:20:51 +0300

    Update ListOfProducts.java

--- BusinessService/Entities/ListOfProducts.java
+++ BusinessService/Entities/ListOfProducts.java
@@ -2,24 +2,29 @@ package BusinessService.Entities;
 
 import java.util.ArrayList;
 
-/**
- * @author Владимир
- * @version 1.0
- * @created 01-дек-2014 23:50:18
- */
-public class ListOfProducts {
-
+public class ListOfProducts
+{
 	private ArrayList<Product> products;
 	public Product m_Product;
 
-
-
-	public void finalize() throws Throwable {
-
+	public ListOfProducts()
+	{
+		products = new ArrayList<>();
 	}
-
-	public void ListOfProducts(){
-
+	public void add(Product p)
+	{
+		products.add(p);
 	}
-
+	public void delete(Product p)
+	{
+		products.remove(p);
+	}
+	public int size()
+	{
+		return products.size();
+	}
+	public Product get(int index)
+	{
+		return products.get(index);
+	}
 }
