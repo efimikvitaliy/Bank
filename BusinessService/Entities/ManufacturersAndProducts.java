33551bba37eddb72eb704c589fe567d2428b5b27
zhukalexey <zhukalexe@mail.ru>
2014-12-04 19:21:18 +0300

    Update ManufacturersAndProducts.java

--- BusinessService/Entities/ManufacturersAndProducts.java
+++ BusinessService/Entities/ManufacturersAndProducts.java
@@ -1,39 +1,55 @@
 package BusinessService.Entities;
 
-/**
- * @author User
- * @version 1.0
- * @created 01-дек-2014 23:50:21
- */
-public class ManufacturersAndProducts {
+public class ManufacturersAndProducts
+{
+	private String manufacturers;
+	private String products;
+	private int id;
+	private int count;
 
-	private String Manufacturers;
-	private String Products;
-
-	public ManufacturersAndProducts(){
-
+	public ManufacturersAndProducts(){}
+	public ManufacturersAndProducts(int id, String manufacturers, String products)
+	{
+		this.id = id;
+		this.manufacturers = manufacturers;
+		this.products = products;
 	}
-
-	public void finalize() throws Throwable {
-
+	public void update(int id, String m, String p)
+	{
+		this.id = id;
+		this.manufacturers = m;
+		this.products = p;
 	}
-
-	/**
-	 * 
-	 * @param Products
-	 * @param Manufacturers
-	 */
-	public void ManufacturersAndProducts(String Products, String Manufacturers){
-
+	public int getId()
+	{
+		return id;
 	}
-
-	/**
-	 * 
-	 * @param m
-	 * @param p
-	 */
-	public void update(String m, String p){
-
+	public void setId(int id)
+	{
+		this.id = id;
 	}
-
+	public String getManufacturers()
+	{
+		return manufacturers;
+	}
+	public void setManufacturers(String manufacturers)
+	{
+		this.manufacturers = manufacturers;
+	}
+	public String getProducts()
+	{
+		return products;
+	}
+	public void setProducts(String products)
+	{
+		this.products = products;
+	}
+	public int getCount()
+	{
+		return count;
+	}
+	public void setCount(int count)
+	{
+		this.count = count;
+	}
 }
