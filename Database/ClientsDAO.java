1a07fc894c0e0201414693437dba76b8b1cd39ad
zhukalexey <zhukalexe@mail.ru>
2014-12-04 19:23:56 +0300

    Update ClientsDAO.java

--- Database/ClientsDAO.java
+++ Database/ClientsDAO.java
@@ -1,26 +1,46 @@
 package Database;
 
-import BusinessService.Entities.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import BusinessService.Entities.Client;
+import BusinessService.Entities.ListOfClients;
 
-
-
-/**
- * @author User
- * @version 1.0
- * @created 01-дек-2014 23:51:10
- */
-public class ClientsDAO {
-
-	public ClientsDAO(){
-
+public class ClientsDAO
+{
+    private Connection con = null;
+    private Statement stmt = null;
+    private ResultSet r = null;
+    
+	public ClientsDAO()
+	{
+		con = DBManager.getInstance().getConnection();
 	}
-
-	public void finalize() throws Throwable {
-
+	public ListOfClients getListOfClients() throws SQLException
+	{
+        stmt = con.createStatement();
+        r = stmt.executeQuery("SELECT * FROM CLIENT");
+        ListOfClients array = new ListOfClients();
+        while(r.next())
+        {
+        	Client m = new Client(r.getString("id"), r.getString("firstName"), r.getString("secondName"));
+        	array.add(m);
+        }
+        r.close();
+        stmt.close();
+		return array;
 	}
-
-	public ListOfClients getListOfClients(){
-		return null;
+	public Client getClient(String id) throws SQLException
+	{
+        stmt = con.createStatement();
+        r = stmt.executeQuery("SELECT * FROM CLIENT WHERE id = " + Integer.valueOf(id));
+        r.next();
+        Client client = new Client(r.getString("id"), r.getString("firstName"), r.getString("secondName"));
+        client.setAddress(r.getString("address"));
+        client.setEmail(r.getString("email"));
+        r.close();
+        stmt.close();
+		return client;
 	}
-
 }
