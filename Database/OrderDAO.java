3927f92639f00af5e384264f6a0d04811561365d
zhukalexey <zhukalexe@mail.ru>
2014-12-04 19:24:44 +0300

    Update OrderDAO.java

--- Database/OrderDAO.java
+++ Database/OrderDAO.java
@@ -1,97 +1,114 @@
 package Database;
 
-import BusinessService.Entities.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import BusinessService.Entities.Catalog;
+import BusinessService.Entities.CatalogRecord;
+import BusinessService.Entities.ListOfOrders;
+import BusinessService.Entities.ManufacturersAndProducts;
+import BusinessService.Entities.Order;
 
-/**
- * @author User
- * @version 1.0
- * @created 01-дек-2014 23:51:13
- */
-public class OrderDAO {
-
-	public OrderDAO(){
+public class OrderDAO
+{
+    private Connection con = null;
+    private Statement stmt = null;
+    private ResultSet r = null;
+	
+	public OrderDAO()
+	{
+		con = DBManager.getInstance().getConnection();
+	}
+	public void deleteOrder(String name)
+	{
 
 	}
-
-	public void finalize() throws Throwable {
-
+	public ListOfOrders getListOfOrder() throws SQLException
+	{
+		ListOfOrders array = new ListOfOrders();
+		stmt = con.createStatement();
+		int order_id = 0;
+		r = stmt.executeQuery("SELECT * FROM TABLE_ORDER, CLIENT WHERE TABLE_ORDER.client_id = CLIENT.id");
+		while(r.next())
+		{
+			Order order = new Order();
+			array.add(order);
+		}
+        r.close();
+        stmt.close();
+		return array;
 	}
-
-	/**
-	 * 
-	 * @param name
-	 */
-	public void deleteOrder(String name){
-
-	}
-
-	public ListOfOrders getListOfOrder(){
+	public ListOfOrders getListOfReturns()
+	{
 		return null;
 	}
-
-	public ListOfOrders getListOfReturns(){
+	public ListOfOrders getListOfSendOrder()
+	{
 		return null;
 	}
-
-	public ListOfOrders getListOfSendOrder(){
+	public Order getOrder(String name)
+	{
 		return null;
 	}
-
-	/**
-	 * 
-	 * @param name
-	 */
-	public Order getOrder(String name){
+	public Order getReturn(String name)
+	{
 		return null;
 	}
-
-	/**
-	 * 
-	 * @param name
-	 */
-	public Order getReturn(String name){
-		return null;
-	}
-
-	/**
-	 * 
-	 * @param orders
-	 */
-	public void saveListOfOrders(ListOfOrders orders){
+	public void saveListOfOrders(ListOfOrders orders)
+	{
 
 	}
-
-	/**
-	 * 
-	 * @param order
-	 */
-	public void saveOrder(Order order){
+	public void saveOrder(Order order) throws SQLException
+	{
+		stmt = con.createStatement();
+		int order_id = 0;
+		r = stmt.executeQuery("SELECT * FROM TABLE_ORDER");
+        while(r.next())
+        {
+        	int temp = Integer.valueOf(r.getString("id")); 
+        	if(order_id < temp)
+        	{
+        		order_id = temp;
+        	}
+        }
+        ++order_id;
+		int client_id = Integer.valueOf(order.getClient().getId());
+        stmt.executeUpdate("INSERT INTO [TABLE_ORDER] ([id], [state], [deliverTransportCompany], [returnTransportCompany], [client_id]) " +
+        		"VALUES(" + order_id + ", 1, 1, 1, " + client_id + ")");
+		r = stmt.executeQuery("SELECT * FROM ORDER_LINE");
+		int id = 0;
+        while(r.next())
+        {
+        	int temp = Integer.valueOf(r.getString("id")); 
+        	if(id < temp)
+        	{
+        		id = temp;
+        	}
+        }
+        ++id;
+        for(int i = 0; i < order.getListOfProducts().size(); ++i)
+        {
+        	stmt.executeUpdate("INSERT INTO [ORDER_LINE] ([id], [order_id], [catalog_record_id], [count]) " + 
+                               "VALUES(" + id + ", " + order_id + ", " + order.getListOfProducts().get(i).getId() + ", " +
+        			           order.getListOfProducts().get(i).getProduct().getCount() + ")");
+        	++id;
+        }
+        
+        r.close();
+        stmt.close();
+	}
+	public void saveOrderWithTransportCompany(Order order)
+	{
 
 	}
-
-	/**
-	 * 
-	 * @param order
-	 */
-	public void saveOrderWithTransportCompany(Order order){
+	public void saveOrderWithTransportCompanyToReturn(Order order)
+	{
 
 	}
-
-	/**
-	 * 
-	 * @param order
-	 */
-	public void saveOrderWithTransportCompanyToReturn(Order order){
+	public void saveReturn(Order order)
+	{
 
 	}
-
-	/**
-	 * 
-	 * @param order
-	 */
-	public void saveReturn(Order order){
-
-	}
-
 }
