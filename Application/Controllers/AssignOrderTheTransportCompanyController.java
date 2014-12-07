8bee6da46d54be14dea7a94ffe21c9c2ba476bb3
efimikvitaliy <vitaliy1993@mail.ru>
2014-12-07 21:26:21 +0300

    Controller in Progress

--- Application/Controllers/AssignOrderTheTransportCompanyController.java
+++ Application/Controllers/AssignOrderTheTransportCompanyController.java
@@ -1,4 +1,8 @@
 package Application.Controllers;
+import java.sql.SQLException;
+
+import javax.swing.JFrame;
+
 import BusinessService.Entities.*;
 import Application.Forms.*;
 import Database.*;
@@ -18,6 +22,7 @@ public class AssignOrderTheTransportCompanyController {
 	public OrderForm m_OrderForm;
 	public ListOfTransportCompanies m_ListOfTransportCompanies;
 	public ListOfOrderForm m_ListOfOrderForm;
+	public JFrame mainFrame;
 
 	public AssignOrderTheTransportCompanyController(){
 
@@ -41,6 +46,16 @@ public class AssignOrderTheTransportCompanyController {
 	 */
 	public void transportCompanySelected(TransportCompany company){
 
+	}
+	
+	public void setMainFrame(JFrame frame){
+		mainFrame = frame;
+	}
+	public void startRoutine() throws SQLException{
+		m_OrderDAO = new OrderDAO();
+		m_ListOfOrders = m_OrderDAO.getListOfOrder();
+		m_ListOfOrderForm.displayListOfOrders(m_ListOfOrders);
+		
 	}
 
 }
